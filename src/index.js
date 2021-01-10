33679a84d9845da1a366a83cd4748597fdbaef0f
Bennett Clement <benclmnt.dev@gmail.com>
2021-01-10 21:31:27 +0800

    Working mvp

---
+++ src/index.js
@@ -0,0 +1,126 @@
+const DEBUG = process.env.NODE_ENV !== 'production'
+
+const escapeHtml = str => str.replace(/</g, '\\u003c')
+const template = body => `
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="">
+    <link rel="icon"
+        href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>🐰</text></svg>">
+    <style>
+    </style>
+    <title>
+        QR Code Attendance
+    </title>
+</head>
+<body>
+  ${body}
+</body>
+</html>
+`
+const form = name => {
+  return name
+    ? template(`Thankyou ${escapeHtml(name)} for attending!`)
+    : template(`
+<h1>Angklung Check-In</h1>
+<form method="post">
+    <label>Name</label>
+    <input type="text" name="name">
+    <label>NUSNET</label>
+    <input type="text" name="nusnet">
+    <input type="submit">
+</form>
+`)
+}
+
+const setCache = (key, value) => ATTENDSYS.put(key, value)
+const getCache = key => ATTENDSYS.get(key)
+
+const isValidTimeRange = datetime => {
+  const hr = datetime.getHours()
+  const min = datetime.getMinutes()
+  // 18.30 - 20.59
+  if (hr < 18 || hr > 21) return false
+  if (hr == 19 || hr == 20) return true
+  if (min < 30) return false
+}
+
+const isValidDay = datetime => {
+  const day = datetime.getDay()
+  return day == 1 || day == 3
+}
+
+async function updateAttendance(request) {
+  let formData = await request.formData()
+  try {
+    const nusnet = escapeHtml(formData.get('nusnet'))
+    const name = escapeHtml(formData.get('name'))
+    const now = new Date()
+    const currDate = now.toLocaleDateString('en-GB', {
+      timeZone: 'Asia/Singapore',
+    }) // "dd/mm/yyyy",
+    const currTime = now.toLocaleTimeString('en-GB', {
+      timeZone: 'Asia/Singapore',
+    }) // "hh:mm:ss"
+
+    if (DEBUG) {
+      if (!isValidTimeRange(now) || !isValidDay(now)) {
+        throw new Error('Sorry, currently this service is unavailable')
+      }
+    }
+
+    let data = await getCache(nusnet)
+    if (!data) {
+      data = {
+        name: [name],
+        attendance: [],
+      }
+    } else {
+      data = JSON.parse(data)
+    }
+
+    if (!data.attendance.find(x => x.date == currDate)) {
+      data.attendance.push({
+        date: currDate,
+        time: currTime,
+      })
+    }
+
+    if (DEBUG) {
+      console.log(JSON.stringify(data.attendance))
+    }
+
+    await setCache(nusnet, JSON.stringify(data))
+    // https://en.wikipedia.org/wiki/Post/Redirect/Get
+    return new Response(null, {
+      status: 302,
+      headers: { location: `http://localhost:8787/?name=${name}` },
+    })
+  } catch (err) {
+    return new Response(err, { status: 500 })
+  }
+}
+
+async function listAttendance(request) {}
+
+/**
+ * Respond with hello worker text
+ * @param {Request} request
+ */
+async function handleRequest(request) {
+  if (request.method === 'POST') {
+    return updateAttendance(request)
+  }
+
+  const url = new URL(request.url)
+  return new Response(form(url.searchParams.get('name')), {
+    headers: { 'content-type': 'text/html' },
+  })
+}
+
+addEventListener('fetch', event => {
+  event.respondWith(handleRequest(event.request))
+})
---
+++ webpack.config.js
@@ -0,0 +1,8 @@
+/* eslint-disable */
+module.exports = {
+  target: 'webworker',
+  entry: './src',
+  module: {
+    rules: [],
+  },
+}
